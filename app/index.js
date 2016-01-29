18dcaa177f1151afe6b402df6828ffb02182f7b3
vikramcse <vikramcse.10@gmail.com>
2016-01-29 21:07:56 +0530

    added bootstrap and some css

--- app/index.js
+++ app/index.js
@@ -1,12 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var App = require('./components/App');
 
-var App = React.createClass({
-  render() {
-    return (
-    <h1>Hello world</h1>
-    );
-  }
-});
+var options = {
+  title: 'Choose a Fruit',
+  items: ['Apple', 'banana', 'mango', 'pineapple']
+};
 
-ReactDOM.render(<App />, document.body);
+ReactDOM.render(<App data={options} />, document.getElementById('app'));
